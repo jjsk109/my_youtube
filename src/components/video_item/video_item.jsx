5cc5b1e0e1f5abd92ab85a5e552deeb6b05ef7a1
jjsk109 <39641848+jjsk109@users.noreply.github.com>
2021-03-31 21:35:56 +0900

    youtube

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import styles from './videio_item.module.css';
+const VideoItem = ({video:{snippet} }) => 
+(  
+    <li className={styles.container}>
+        <div className={styles.video}>
+            <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumnail"/>
+            <div className={styles.metadata}>
+            <p className={styles.title}>{snippet.title}</p>
+            <p className={styles.channel}>{snippet.channelTitle}</p>
+            </div>
+        </div>
+      
+    </li>
 
-const VideoItem = props => <h1>{props.video.snippet.title}</h1>
-
+)
 export default VideoItem;
